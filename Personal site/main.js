c9b64f3e4d91010e6fd1c52a92d8cb8b90433706
Viann Cheng <46937676+vivchc@users.noreply.github.com>
2023-05-11 01:12:47 -0400

    Delete Personal site directory

--- Personal site/main.js
+++
@@ -1,5 +0,0 @@
-import './style.css';
-import Experience from './Experience/Experience';
-
-/* Create canvas for Three.js */
-const experience = new Experience(document.querySelector('.experience-canvas'));
