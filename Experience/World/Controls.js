d9ce1d20211a7a57d39056b93c2a72e827e66375
Viann Cheng <46937676+vivchc@users.noreply.github.com>
2023-05-16 21:50:56 -0400

    Added mailbox animations

--- Experience/World/Controls.js
+++ Experience/World/Controls.js
@@ -238,7 +238,96 @@ export default class Controls {
                     });
             },
             // all
-            all: () => {}
+            all: () => {
+                // Animate mailbox platform
+                this.mailboxTimeline = new GSAP.timeline({
+                    scrollTrigger: {
+                        trigger: '.third-scrollTrigger',
+                        markers: true,
+                        start: 'center center'
+                    }
+                });
+
+                // Loop through children; this.room.children may render in arbitrary order
+                this.room.children.forEach((roomChild) => {
+                    if (roomChild.name === 'mailbox') {
+                        roomChild.children.forEach((child) => {
+                            // Animate mailbox floor extending out
+                            if (child.name.includes('floor')) {
+                                this.one = GSAP.to(child.position, {
+                                    // Final position relative to starting position in model
+                                    x: 0,
+                                    z: 0, // equi. to y in Blender
+                                    // note: adjust duration
+                                    duration: 0.3 // lower = faster
+                                });
+                            }
+
+                            // Animate steps (3 total)
+                            if (child.name.includes('step_01')) {
+                                this.two = GSAP.to(child.scale, {
+                                    // Final size relative to WITHIN the scene
+                                    x: 5.1,
+                                    y: 5.1,
+                                    z: 5.1,
+                                    ease: 'back.out(2)', // obj expands then shrinks to actual size
+                                    duration: 0.5 // lower = faster
+                                });
+                            }
+                            if (child.name.includes('step_02')) {
+                                this.three = GSAP.to(child.scale, {
+                                    // Final size relative to WITHIN the scene
+                                    x: 5.1,
+                                    y: 5.1,
+                                    z: 5.1,
+                                    ease: 'back.out(2)', // obj expands then shrinks to actual size
+                                    duration: 0.5 // lower = faster
+                                });
+                            }
+                            if (child.name.includes('step_03')) {
+                                this.four = GSAP.to(child.scale, {
+                                    // Final size relative to WITHIN the scene
+                                    x: 5.1,
+                                    y: 5.1,
+                                    z: 5.1,
+                                    ease: 'back.out(2)', // obj expands then shrinks to actual size
+                                    duration: 0.5 // lower = faster
+                                });
+                            }
+
+                            // Animate mailbox itself
+                            if (child.name.includes('stand')) {
+                                this.five = GSAP.to(child.scale, {
+                                    // Final size relative to WITHIN the scene
+                                    x: 5.1,
+                                    y: 5.1,
+                                    z: 5.1,
+                                    ease: 'back.out(2)', // obj expands then shrinks to actual size
+                                    duration: 0.5 // lower = faster
+                                });
+                            }
+                            if (child.name.includes('body')) {
+                                this.six = GSAP.to(child.scale, {
+                                    // Final size relative to WITHIN the scene
+                                    x: 5.1,
+                                    y: 5.1,
+                                    z: 5.1,
+                                    ease: 'back.out(2)', // obj expands then shrinks to actual size
+                                    duration: 0.5 // lower = faster
+                                });
+                            }
+                        });
+
+                        // Animate children in order
+                        this.mailboxTimeline.add(this.one); // floor
+                        this.mailboxTimeline.add(this.two, '-=0.2'); // step1
+                        this.mailboxTimeline.add(this.three, '-=0.2'); // step2
+                        this.mailboxTimeline.add(this.four, '-=0.2'); // step3
+                        this.mailboxTimeline.add(this.five, '-=0.2'); // mailbox stand
+                        this.mailboxTimeline.add(this.six, '-=0.2'); // mailbox body
+                    }
+                });
+            }
         });
     }
 
